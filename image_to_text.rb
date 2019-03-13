4cd9a06449d40b448fc493a165655a1b2848e04d
Todd Metheny <todd.metheny@gmail.com>
2019-03-13 16:27:30 -0400

    added readme that doesn't say anything

--- image_to_text.rb
+++ image_to_text.rb
@@ -1,47 +1,41 @@
-require 'cloudmersive-ocr-api-client'
-require 'open-uri'
-# move this to its own file
-class ImageToText
+require "cloudmersive-ocr-api-client"
+require "open-uri"
 
-    def initialize(file, is_url=false)
-      CloudmersiveOcrApiClient.configure do |config|
-        config.api_key['Apikey'] = ENV['CLOUDMERSIVE_API_KEY']
-      end
-  
-      @api_instance = CloudmersiveOcrApiClient::ImageOcrApi.new
-  
-      @file = file
-      @is_url = is_url
+class ImageToText
+  def initialize(file, is_url = false)
+    CloudmersiveOcrApiClient.configure do |config|
+      config.api_key["Apikey"] = ENV["CLOUDMERSIVE_API_KEY"]
     end
-  
-    def image_file
-      if @is_url
-        image_file = open('image.png', 'wb') do |file|
-          file << open(@file).read
-        end
-      else # it's a path instead
-        @image_file = File.new(@file) # File | Image file to perform OCR on.  Common file formats such as PNG, JPEG are supported.
-      end
-    end
-  
-    def fetch_text
-      opts = { 
-        language: "ENG",
-        preprocessing: "Auto"
-      }
-    
-      begin
-        result = @api_instance.image_ocr_post(self.image_file, opts)
-        
-        if result.text_result.empty?
-          return ''
-        else
-          text_result = result.text_result.gsub("\n", "")
-          return text_result
-        end
-      rescue CloudmersiveOcrApiClient::ApiError => e
-        puts "Exception when calling ImageOcrApi->image_ocr_post: #{e}"
-      end
-    end
-  
+
+    @api_instance = CloudmersiveOcrApiClient::ImageOcrApi.new
+    @opts = {language: "ENG", preprocessing: "Auto"}
+
+    @file = file
+    @is_url = is_url
   end
+
+  def image_file
+    if @is_url
+      image_file = open("image.png", "wb") do |file|
+        file << open(@file).read
+      end
+    else # it's a path instead
+      @image_file = File.new(@file) # File | Image file to perform OCR on.  Common file formats such as PNG, JPEG are supported.
+    end
+  end
+
+  def fetch_text
+    begin
+      result = @api_instance.image_ocr_post(self.image_file, @opts)
+
+      if result.text_result.empty?
+        return ""
+      else
+        text_result = result.text_result.gsub("\n", "")
+        return text_result
+      end
+    rescue CloudmersiveOcrApiClient::ApiError => e
+      puts "Exception when calling ImageOcrApi->image_ocr_post: #{e}"
+    end
+  end
+end
--- ocr_test.rb
+++ ocr_test.rb
@@ -1,36 +1,36 @@
-require 'sinatra'
-require 'json'
+require "sinatra"
+require "json"
 
-require './image_to_text.rb'
-require './send_to_s3.rb'
-require './search.rb'
+require "./image_to_text.rb"
+require "./send_to_s3.rb"
+require "./search.rb"
 
 # sinatra routes
-get '/' do
-  send_file 'public/ocr_test.html'
+get "/" do
+  send_file "public/ocr_test.html"
 end
 
-post '/get_image_url' do
-  file = params['file']
-  original_name = file['filename']
-  
+post "/get_image_url" do
+  file = params["file"]
+  original_name = file["filename"]
+
   if original_name.match(/\s/)
     original_name = original_name.tr(" ", "_")
   end
-  
+
   filename = "ocr_test/#{original_name}"
 
   puts "filename: #{filename}"
-  
+
   to_s3 = SendToS3.new(file)
   response_hsh = to_s3.upload
 
   # get text from image
   ocr_image = ImageToText.new(response_hsh[:url], true)
   text_from_image = ocr_image.fetch_text
-  
+
   response_hsh[:text_from_image] = text_from_image
-  
+
   # search the text you got from the image
   unless text_from_image.empty?
     search = Search.new(text_from_image)
@@ -38,10 +38,9 @@ post '/get_image_url' do
   else
     results = []
   end
-  
+
   response_hsh[:results] = results
 
   content_type :json
   response_hsh.to_json
 end
-
